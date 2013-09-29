a8e57d04879c43821b3abb52a4168408848dfb38
xmik3yvx <mikeyvilla81@gmail.com>
2013-09-29 01:02:27 -0700

    linked post-user, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Blog::Application.routes.draw do
-  resources :posts
-
-  resources :users
 
   get "home/index"
+
+  resources :users do
+    resources :posts    
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
