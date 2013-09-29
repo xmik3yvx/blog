229d68b1359f4322d309bbc306abe0f02bbda5cf
xmik3yvx <mikeyvilla81@gmail.com>
2013-09-29 02:00:32 -0700

    user validations/email index db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  before_save do
+    self.email = email.downcase 
+	self.username = username.downcase
+  end
+  
+  validates :username, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130929074724) do
+ActiveRecord::Schema.define(version: 20130929085051) do
 
   create_table "posts", force: true do |t|
     t.string   "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20130929074724) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
