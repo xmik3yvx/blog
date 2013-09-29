a5b024d2176aa6682947edcdc74e1568af62cdf3
xmik3yvx <mikeyvilla81@gmail.com>
2013-09-29 14:24:21 -0700

    added password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130929085051) do
+ActiveRecord::Schema.define(version: 20130929201822) do
 
   create_table "posts", force: true do |t|
     t.string   "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20130929085051) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
